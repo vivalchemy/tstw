3b0455e1cbb7ac70e37610926bf5e779c46d898d
vivianludrick <vivianludrick.social+github@gmail.com>
2023-08-05 23:12:07 +0530

    github bg

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tstw
+
 Websites made using html, tailwind and typescript(or javascript)
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./tailwind_only/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
